4e41ef9fb31ffa995cf79dc0cca32bb7ce69adb7
Ugochukwu Nnamani <113511475+Dontech021@users.noreply.github.com>
2023-03-14 09:13:07 +0100

    Update Churn_app.py

--- Churn_app.py
+++ Churn_app.py
@@ -1,6 +1,8 @@
 import subprocess
 subprocess.call(['python', '--version'])
+subprocess.call(['python -c \"import joblib; print(joblib.file)\"'])
 
+python -c "import joblib; print(joblib.file)"
 import streamlit as st
 import pandas as pd
 import gzip
