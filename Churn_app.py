96f62b85545f8862deff2487e7521777df496ed5
Ugochukwu Nnamani <113511475+Dontech021@users.noreply.github.com>
2023-03-15 12:18:59 +0100

    Update Churn_app.py

--- Churn_app.py
+++ Churn_app.py
@@ -10,7 +10,7 @@ import numpy as np
 
 st.write("""
 # Churn Prediction App
-click "**<**" on the left hand corner and choose your parameters from the sidebar that appears to know if customer will **churn**
+click "**> button**" on the left hand corner and choose your parameters from the sidebar that appears to know if customer will **churn**
 """)
 
 
