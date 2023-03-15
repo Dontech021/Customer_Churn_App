293de64e4f9ba14936e66fb83e21f31b7be208e1
Ugochukwu Nnamani <113511475+Dontech021@users.noreply.github.com>
2023-03-15 09:03:02 +0100

    Update Churn_app.py

--- Churn_app.py
+++ Churn_app.py
@@ -44,7 +44,9 @@ st.subheader('Prediction Parameters')
 st.write(df)
 
 try:
-    st.write('Previous result', pd.DataFrame({'won\'t churn':pred_prob[0][0],'churn':pred_prob[0][1]},index=['probability']))
+    st.write( pd.DataFrame({'won\'t churn':pred_prob[0][0],'churn':pred_prob[0][1]},index=['probability']))
+    st.write('Previous result')
+
 except:
     st.write('click the predict button below to make prediction')
 button_clicked=False
